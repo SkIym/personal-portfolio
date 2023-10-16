bd0e93abed95972799ef9d8fc3152314918cc6ee
SkIym <abramjosh13@gmail.com>
2023-10-16 19:22:33 +0800

    Improve box layouts + responsiveness

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './assets/style.css';
+
+const dropDownCnt = document.querySelector('.dropdown-content');
+const dropDownBtn = document.querySelector('#dropdown-button');
+
+function showDropDown() {
+  console.log("assad")
+  dropDownCnt.classList.toggle('show');
+}
+
+dropDownBtn.onclick = showDropDown;
